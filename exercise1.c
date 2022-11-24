2cb87d5766b1899ab35fe4639eb65ad256b54f3f
rcseven <rcedquila@gmail.com>
2022-11-25 01:11:35 +0800

    i/o syntax

--- exercise1.c
+++ exercise1.c
@@ -1,7 +1,32 @@
-#include<stio.h>
+#include<stdio.h>
 
 int main() {
+    // initialize variables
     char charVal;
+    char name[9];
     int intVal;
+
+    // Input syntax
     
+    // Character
+    printf("Enter Character: ");
+    scanf("%c", &charVal);
+    
+
+    // String
+    printf("Enter Name: ");
+    scanf("%s", name);
+
+    // Integer
+    printf("Enter a Integer: ");
+    scanf("%d", &intVal);
+
+    
+
+    // Output
+    printf("Char: %c\n", charVal);
+    printf("String: %s\n", name);
+    printf("Integer: %d\n", intVal);
+
+    return 0;
 }

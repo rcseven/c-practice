04b3862440bb990eb5a7ebf47c96d966f2d04e8d
rcseven <rcedquila@gmail.com>
2022-11-29 01:35:07 +0800

    traingle

---
+++ triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    int size;
+    printf("triangle size: ");
+    scanf("%d", &size);
+
+    for (int i = 1; i < size; i++) {
+        for (int j = 1; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+
+    return 0;
+}

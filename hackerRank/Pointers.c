6d011dab80de667f821940cc484a440aa7ebd3f6
rcseven <rcedquila@gmail.com>
2022-12-03 22:49:07 +0800

    pointers

---
+++ hackerRank/Pointers.c
@@ -0,0 +1,28 @@
+// Task
+
+// Complete the function void update(int *a,int *b). 
+// It receives two integer pointers, int* a and int* b. 
+// Set the value of  to their sum, and  to their absolute difference. 
+// There is no return value, and no return statement is needed.
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+void update(int *a,int *b) {
+    int c = *a+*b;
+    *b = abs(*a-*b);
+    *a = c;    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
